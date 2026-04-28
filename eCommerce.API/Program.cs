f61f8d9b856c818f713db61634de54bebc60c5e5
Arvis Iljins <arvis.iljins@elkogroup.com>
2026-04-28 20:00:57 +0300

    Add Swagger for API documentation and configure CORS policy

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -17,10 +17,25 @@ builder
     });
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfiles>());
 builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+});
 app.UseAuthorization();
 app.UseAuthentication();
 app.MapControllers();
